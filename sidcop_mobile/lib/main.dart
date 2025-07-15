003f984e1747bb4244bc221d2e5f393c1352c208
Oscar Valdivieso <fernandoscar04@gmail.com>
2025-07-15 11:02:33 -0600

    Reapply "feat(configure folders and environment)"
    This reverts commit 46b58d54f69181dac82a9211efb5fb1a3c39b396.

--- sidcop_mobile/lib/main.dart
+++ sidcop_mobile/lib/main.dart
@@ -1,20 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+import 'app.dart';
+Future<void> main() async {
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  await dotenv.load(fileName: '.env');
+  runApp(const MyApp());
 }
